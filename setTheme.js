48e64b7241eae95c48997469fc66f1e17feceb4a
Walysson Paiva <walyssonpaiva@outlook.com>
2021-08-13 15:34:19 -0300

    refactor: storage theme in json file and replacing css var

--- setTheme.js
+++ setTheme.js
@@ -1,46 +1,15 @@
 chrome.storage.sync.get(['currentTheme'], aplyingTheme)
 
 function aplyingTheme(currentTheme){
-    //default colors
-    var lv1 = 'fill: #FF9CD4 !important;'
-    var lv2 = 'fill: #F780C8 !important;'
-    var lv3 = 'fill: #F059A5 !important;'
-    var lv4 = 'fill: #E0218A !important;'
-    if(currentTheme.currentTheme == 'Unicorn'){
-        var lv1 = 'fill: #FF72C1 !important;'
-        var lv2 = 'fill: #64F651 !important;'
-        var lv3 = 'fill: #A05BFD !important;'
-        var lv4 = 'fill: #15DBFF !important;'
-    } else if(currentTheme.currentTheme == 'Light_Blue'){
-        var lv1 = 'fill: #30BFBF !important;'
-        var lv2 = 'fill: #26B0BF !important;'
-        var lv3 = 'fill: #0C9ABA !important;'
-        var lv4 = 'fill: #0689AE !important;'
-    }else if(currentTheme.currentTheme == 'Pink'){
-        var lv1 = 'fill: #FF9CD4 !important;'
-        var lv2 = 'fill: #F780C8 !important;'
-        var lv3 = 'fill: #F059A5 !important;'
-        var lv4 = 'fill: #E0218A !important;'
-    }else if(currentTheme.currentTheme == 'Dark_Blue'){
-        var lv1 = 'fill: #679cd0 !important;'
-        var lv2 = 'fill: #4183c4 !important;'
-        var lv3 = 'fill: #31699f !important;'
-        var lv4 = 'fill: #254f77 !important;'
-    }
-    const level4 = document.querySelectorAll("rect[data-level='4']")
-        for( element of level4)
-            element.setAttribute("style", lv4)
-    
-        const level3 = document.querySelectorAll("rect[data-level='3']")
-        for( element of level3)
-            element.setAttribute("style", lv3)
-    
-        const level2 = document.querySelectorAll("rect[data-level='2']")
-        for( element of level2)
-            element.setAttribute("style", lv2)
-    
-        const level1 = document.querySelectorAll("rect[data-level='1']")
-        for( element of level1)
-            element.setAttribute("style", lv1)
 
+    fetch(chrome.extension.getURL('./themes.json'))
+    .then((resp) => resp.json())
+    .then(function (jsonData) {
+        const { lv1, lv2, lv3, lv4 } = jsonData[currentTheme.currentTheme];
+        const page = document.querySelector(':root');
+        page.style.setProperty('--color-calendar-graph-day-L1-bg', lv1)
+        page.style.setProperty('--color-calendar-graph-day-L2-bg', lv2)
+        page.style.setProperty('--color-calendar-graph-day-L3-bg', lv3)
+        page.style.setProperty('--color-calendar-graph-day-L4-bg', lv4)
+    });
 }
