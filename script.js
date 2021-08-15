d5f6bf70c06e52d986b25ac605ce7979b56a722e
Walysson Paiva <walyssonpaiva@outlook.com>
2021-08-15 00:57:40 -0300

    refactor: enforce code style with single quotes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-button = document.querySelector("#select");
-button.addEventListener("click", selectTheme);
-button.addEventListener("mouseout", onMouseOut);
-button.addEventListener("mouseover", onMouseOver);
+button = document.querySelector('#select');
+button.addEventListener('click', selectTheme);
+button.addEventListener('mouseout', onMouseOut);
+button.addEventListener('mouseover', onMouseOver);
 
 function onMouseOut() {
-  chbg("1px solid #3f3d56");
+  chbg('1px solid #3f3d56');
 }
 
 function onMouseOver() {
-  chbg("1px solid #ff63a6");
+  chbg('1px solid #ff63a6');
 }
 
 function selectTheme() {
@@ -16,7 +16,7 @@ function selectTheme() {
   var value = select.options[select.selectedIndex].value;
 
   chrome.storage.sync.set({ currentTheme: value }, function () {
-    console.log("Settings saved");
+    console.log('Settings saved');
   });
 }
 
@@ -24,11 +24,11 @@ function setCurrentTheme(currentTheme) {
   const current = currentTheme.currentTheme;
   var select = document.querySelector(`[value=${current}]`);
 
-  if (current != undefined) select.setAttribute("selected", "selected");
+  if (current != undefined) select.setAttribute('selected', 'selected');
 }
 
 function chbg(color) {
-  document.getElementById("themes").style.border = color;
+  document.getElementById('themes').style.border = color;
 }
 
-chrome.storage.sync.get(["currentTheme"], setCurrentTheme);
+chrome.storage.sync.get(['currentTheme'], setCurrentTheme);
