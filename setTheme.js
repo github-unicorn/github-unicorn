789f32804bcdfb16b93e9085c3f74a964ec451a6
odlave <evaldo.felipe2@gmail.com>
2021-11-01 23:28:15 -0300

    feat: apply theme on progress bar

--- setTheme.js
+++ setTheme.js
@@ -20,10 +20,10 @@ function setProperties(theme) {
 	const page = document.querySelector(":root");
 	const jsHgBlob = document.getElementsByClassName("js-highlight-blob");
 	const acOvAxis = document.getElementsByClassName("activity-overview-axis");
-	const acOvPoint = document.getElementsByClassName(
-		"activity-overview-point"
-	);
+	const acOvPoint = document.getElementsByClassName("activity-overview-point");
+	const progressBar = document.getElementsByClassName("Progress-item");
 
+	// apply colors in github contribution calendar graph
 	if (jsHgBlob[0]) {
 		for (let i = 0, x = jsHgBlob.length; i < x; i++) {
 			jsHgBlob[i].style.fill = theme.lv1;
@@ -31,12 +31,14 @@ function setProperties(theme) {
 		}
 	}
 
+	// apply colors in activity overview axis
 	if (acOvAxis[0]) {
 		for (let j = 0, y = acOvAxis.length; j < y; j++) {
 			acOvAxis[j].style.stroke = theme.lv2;
 		}
 	}
 
+	// apply colors in activity overview points
 	if (acOvPoint[0]) {
 		for (let k = 0, z = acOvPoint.length; k < z; k++) {
 			acOvPoint[k].style.fill = theme.lv1;
@@ -44,10 +46,40 @@ function setProperties(theme) {
 		}
 	}
 
+	// apply colors in progress bars
+	if (progressBar[0]) {
+		for (let l = 0, w = progressBar.length; l < w; l++) {
+			let percentage =  parseInt(progressBar[l].style.width.replace("%",""));
+
+			if (percentage > 0 && percentage <= 25) {
+				progressBar[l].style.backgroundColor = theme.lv1;
+			}
+
+			if (percentage > 25 && percentage <= 50) {
+				progressBar[l].style.backgroundColor = theme.lv2;
+			}
+
+			if (percentage > 50 && percentage <= 80) {
+				progressBar[l].style.backgroundColor = theme.lv3;
+			}
+
+			if (percentage > 80 && percentage <= 100) {
+				progressBar[l].style.backgroundColor = theme.lv4;
+			}
+		}
+	}
+
+	// apply colors when isn't holydays
 	page.style.setProperty("--color-calendar-graph-day-L1-bg", theme.lv1);
 	page.style.setProperty("--color-calendar-graph-day-L2-bg", theme.lv2);
 	page.style.setProperty("--color-calendar-graph-day-L3-bg", theme.lv3);
 	page.style.setProperty("--color-calendar-graph-day-L4-bg", theme.lv4);
+
+	// apply colors when are Halloween
+	page.style.setProperty("--color-calendar-halloween-graph-day-L1-bg", theme.lv1);
+	page.style.setProperty("--color-calendar-halloween-graph-day-L2-bg", theme.lv2);
+	page.style.setProperty("--color-calendar-halloween-graph-day-L3-bg", theme.lv3);
+	page.style.setProperty("--color-calendar-halloween-graph-day-L4-bg", theme.lv4);
 }
 
 (() => {
